ddabf9e6959125e7323cb7e93cfa4b58927d78b9
Ilia Bykov <fagotist96@icloud.com>
2024-08-10 00:23:49 +0300

    переменная для пути создания заказа

--- src/services/API.ts
+++ src/services/API.ts
@@ -1,7 +1,7 @@
-import { ConstructorItemIgridient, Res } from "../types/types"
+import { Res } from "../types/types"
 import { BASE_PATH, ORDER_PATH } from "../utils/constants"
 import { setOrder } from "./constructor/orderCostSlice"
-import { AppDispatch, useAppDispatch } from "./store"
+import { AppDispatch } from "./store"
 
 export const getIngridients = async() => {
   try {
@@ -15,28 +15,27 @@ export const getIngridients = async() => {
 
 export const createOrder = async(dispatch: AppDispatch, ingredients: string[]) => {
   try {
-    const response = await fetch('https://norma.nomoreparties.space/api/orders', {
+    const response = await fetch(ORDER_PATH, {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json"
       },
       body: JSON.stringify({ ingredients })
     });
     //позже добавлю обработку ошибок и их отображение в модальном окне
     if(ingredients.length === 0) {
-      alert("Заказ пуст, соберите бургер");
-      return
+      throw new Error("Заказ пуст, соберите бургер");
     }
 
     if (!response.ok) {
-      throw new Error('Ошибка при отправки запроса на создание заказа');
+      throw new Error("Ошибка при отправки запроса на создание заказа");
     }
 
     const data = await response.json();
     if (data.success) {
       dispatch(setOrder({number: data.order.number, name: data.name}));
     } else {
-      throw new Error('Ошибка при создании заказа', data);
+      throw new Error("Ошибка при создании заказа", data);
     }
 
   } catch (error) {
